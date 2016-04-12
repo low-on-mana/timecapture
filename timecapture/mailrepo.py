f07691ed9e7d7fd11ef116bbe8470ab5e8ede419
Anoop Narang <anoop_narang2003@yahoo.com>
2016-04-12 23:24:10 +0530

    removing hardcoding of username in mailrepo

--- timecapture/mailrepo.py
+++ timecapture/mailrepo.py
@@ -29,8 +29,8 @@ def get_credentials():
     Returns:
         Credentials, the obtained credential.
     """
-    # home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join('/home/anoop/', '.credentials')
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
     credential_path = os.path.join(credential_dir,
